b3fab3ca4bab4b312b15bbdb5b7ec7b2b7a3edc7
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-10-29 17:07:34 +0300

    forms and user accounts

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/update',BlogUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import *
+from django.urls import reverse_lazy
+
+
 # Create your views here.
 class BlogListView(ListView):
     template_name = 'home.html'
     model = Post
 
+
 class BlogDetailView(DetailView):
     template_name = "post_detail.html"
     model = Post
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
